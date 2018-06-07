e00eb0c5476721ad0ccb94d92ad23e6052e16cdb
JiamingTian1 <kujoukaren5424@outlook.com>
2018-06-08 02:24:58 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-try:
-    while True:
-        print(0)
-finally:
-    print(123)
+import Function
+data = Function.register('TianAndrew','123').strip().decode()
+print(data)
+
+data = Function.login('TianAndrew','123').strip().decode()
+if data == "1":
+    print("成功登陆")
+
+if data == "2":
+    print("账号错误")
+
+if data == "3":
+    print("密码错误")
+
+data = Function.sendURL('TianAndrew','www.baidu.con/test/test.txt').strip().decode()
+print(data)
+
+data = Function.sendURL('TianAndrew','www.google.con/test/Game.db').strip().decode()
+print(data)
+
+data = Function.getURL('TianAndrew','test.txt').strip().decode()
+print(data)
+
+data = Function.getList('TianAndrew').strip().decode()
+print(data)
+
+#data = Function.delete('TianAndrew','fileName').strip().decode()
+#print(data)
